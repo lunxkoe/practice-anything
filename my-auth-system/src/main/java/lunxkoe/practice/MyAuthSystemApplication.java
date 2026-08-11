7518462da6a8891595231e2bc82f1c77212e43f5
lunxkoe <lunxkoe@gmail.com>
2026-08-11 14:11:52 +0900

    chore: project init

---
+++ my-auth-system/src/main/java/lunxkoe/practice/MyAuthSystemApplication.java
@@ -0,0 +1,13 @@
+package lunxkoe.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAuthSystemApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyAuthSystemApplication.class, args);
+  }
+
+}
