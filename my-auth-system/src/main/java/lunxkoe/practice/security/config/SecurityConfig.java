58481219d34053720a2bc1527f3fd8550f1e7a9e
lunxkoe <lunxkoe@gmail.com>
2026-08-12 14:08:11 +0900

    feat: SecurityConfig 추가

---
+++ my-auth-system/src/main/java/lunxkoe/practice/security/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package lunxkoe.practice.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    http.formLogin(AbstractHttpConfigurer::disable);
+    http.httpBasic(AbstractHttpConfigurer::disable);
+    http.logout(AbstractHttpConfigurer::disable);
+
+    http.csrf(AbstractHttpConfigurer::disable);
+    // TODO: CSRF 설정 추가
+
+    http.sessionManagement(session -> session
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+    );
+
+    // TODO: TokenAuthentication Filter 설정 추가
+
+    http.authorizeHttpRequests(auth -> auth
+        .anyRequest().permitAll()
+    );
+
+    // TODO: Security Exception Handler 설정 추가
+
+    // TODO: OAuth2 설정 추가
+
+    return http.build();
+  }
+}
